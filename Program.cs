f83e1d237a18a2a02a00644f742913348384560e
Allan Aleluia <allan.souza92@hotmail.com>
2023-03-28 14:29:21 +0100

    Exemplo do uso de arquivos estáticos

--- Program.cs
+++ Program.cs
@@ -7,44 +7,63 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+ConfigureMvc(builder);
+ConfigureServices(builder);
 
-builder.Services.
+var app = builder.Build();
+LoadConfiguration(app);
+ConfigureAuthentication(builder);
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+void LoadConfiguration(WebApplication app)
+{
+
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtConfiguration(); //converte a classe smtp
+    app.Configuration.GetSection("Smtp").Bind(smtp); //popula a classe smtp através do Json
+    Configuration.Smtp = smtp; //passa o valor do smtp para a configuração
+
+}
+
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services.
     AddControllers().
     ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
     });
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenService>(); //sempre criar um novo
+}
 
-
-var app = builder.Build();
-
-
-Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
-Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
-Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
-
-
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>(); //sempre criar um novo
+}
